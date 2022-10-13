6f514726e907bc1bfd00101ee2fd8d305b65b422
SachaWheeler <sacha@sachawheeler.com>
2022-10-13 13:55:47 +0100

    create tables and foreign keys

--- create_tables.py
+++ create_tables.py
@@ -7,23 +7,43 @@ from config import config
 def create_tables():
     """ create tables in the PostgreSQL database"""
     commands = (
+        " DROP TABLE IF EXISTS pub CASCADE",
+        " DROP TABLE IF EXISTS location CASCADE",
+        " DROP TABLE IF EXISTS distance CASCADE",
+
         """
         CREATE TABLE pub (
             id SERIAL PRIMARY KEY,
             name VARCHAR(50) NOT NULL,
             address VARCHAR(255) NOT NULL,
             postcode VARCHAR(10) NOT NULL,
-            local VARCHAR(20) NOT NULL
+            borough VARCHAR(20) NOT NULL
         )
         """,
-        """ CREATE TABLE lat_long (
-                pub_id SERIAL PRIMARY KEY,
-                lat float,
-                long float,
-                CONSTRAINT fk_lat_long_pub
-                    FOREIGN KEY(pub_id)
-                    REFERENCES pub(id)
-                )
+        """
+        CREATE TABLE location (
+            pub_id SERIAL PRIMARY KEY,
+            lat float,
+            long float,
+            CONSTRAINT fk_location_pub
+                FOREIGN KEY(pub_id)
+                REFERENCES pub(id)
+            )
+        """,
+        """
+        CREATE TABLE distance (
+            id SERIAL PRIMARY KEY,
+            start_loc integer,
+            end_loc integer,
+            CONSTRAINT fk_start_loc
+                FOREIGN KEY(start_loc)
+                REFERENCES location(pub_id)
+                ON DELETE CASCADE,
+            CONSTRAINT fk_end_loc
+                FOREIGN KEY(end_loc)
+                REFERENCES location(pub_id)
+                ON DELETE CASCADE
+            )
         """
     )
     conn = None
