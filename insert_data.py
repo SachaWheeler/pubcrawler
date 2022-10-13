7e527031757ec67a10b4fcf94ce5e203bc997dff
SachaWheeler <sacha@sachawheeler.com>
2022-10-13 14:06:15 +0100

    create tables and foreign keys

--- insert_data.py
+++ insert_data.py
@@ -4,23 +4,25 @@ import psycopg2
 from config import config
 
 
-def insert_vendor(vendor_name):
-    """ insert a new vendor into the vendors table """
-    sql = """INSERT INTO vendors(vendor_name)
-             VALUES(%s) RETURNING vendor_id;"""
+def insert_pub(pub):
+    """ insert a new pub into the pub table """
+    name = pub['name']
+    address = pub['address']
+    postcode = pub['postcode']
+    borough = pub['borough']
+    sql = """INSERT INTO pub(name, address, postcode, borough)
+             VALUES(%s) RETURNING id;"""
     conn = None
     vendor_id = None
     try:
         # read database configuration
         params = config()
-        # connect to the PostgreSQL database
         conn = psycopg2.connect(**params)
-        # create a new cursor
         cur = conn.cursor()
-        # execute the INSERT statement
-        cur.execute(sql, (vendor_name,))
+
+        cur.execute(sql, (name, address, postcode, borough))
         # get the generated id back
-        vendor_id = cur.fetchone()[0]
+        pub_id = cur.fetchone()[0]
         # commit the changes to the database
         conn.commit()
         # close communication with the database
@@ -33,38 +35,6 @@ def insert_vendor(vendor_name):
 
     return vendor_id
 
-def insert_vendor_list(vendor_list):
-    """ insert multiple vendors into the vendors table  """
-    sql = "INSERT INTO vendors(vendor_name) VALUES(%s)"
-    conn = None
-    try:
-        # read database configuration
-        params = config()
-        # connect to the PostgreSQL database
-        conn = psycopg2.connect(**params)
-        # create a new cursor
-        cur = conn.cursor()
-        # execute the INSERT statement
-        cur.executemany(sql,vendor_list)
-        # commit the changes to the database
-        conn.commit()
-        # close communication with the database
-        cur.close()
-    except (Exception, psycopg2.DatabaseError) as error:
-        print(error)
-    finally:
-        if conn is not None:
-            conn.close()
-
 if __name__ == '__main__':
     # insert one vendor
     insert_vendor("3M Co.")
-    # insert multiple vendors
-    insert_vendor_list([
-        ('AKM Semiconductor Inc.',),
-        ('Asahi Glass Co Ltd.',),
-        ('Daikin Industries Ltd.',),
-        ('Dynacast International Inc.',),
-        ('Foster Electric Co. Ltd.',),
-        ('Murata Manufacturing Co. Ltd.',)
-    ])
