644e7389657b10fa87822e6a84f35e82d1ceb1f3
Sacha Wheeler <sacha@sachawheeler.com>
2024-07-27 09:58:25 +0100

    django start  poiunt

--- sql.sh
+++
@@ -1,7 +0,0 @@
- cat <<EOF | psql pubcrawler
-        SELECT p.name, p.address, d.distance from pub p, distance d
-        WHERE p.id = d.end_loc
-        AND d.start_loc = 11189
-        ORDER BY d.distance
-        LIMIT 10
-EOF
