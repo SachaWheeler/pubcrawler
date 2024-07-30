7be15559a189ec3aec84cc8b5bd05ee1b505ceb1
Sacha Wheeler <sacha@sachawheeler.com>
2024-07-30 09:16:04 +0100

    display maps

---
+++ pubcrawler/pubs/management/commands/display_map.py
@@ -0,0 +1,34 @@
+import osmnx as ox
+import networkx as nx
+from geopy.distance import geodesic, great_circle
+from django.core.management.base import BaseCommand
+from pubs.models import Pub, Distance
+from django.db.models import Q
+from os.path import exists
+import matplotlib.pyplot as plt
+
+import time
+
+
+class Command(BaseCommand):
+    help = 'Calculate distances between pubs'
+
+    def handle(self, *args, **kwargs):
+
+        # graph_file = "/home/sacha/work/pubcrawler/demo/maps/2_London.graphml"
+        graph_file = "/home/sacha/work/pubcrawler/demo/maps/London_central.graphml"
+
+        if exists(graph_file):
+            print(f"Loading mapfile: {graph_file}")
+            st = int(time.time())
+            G = ox.load_graphml(graph_file)
+            et = int(time.time())
+            print(f'mapfile loaded: {et - st} seconds')
+        else:
+            print(f"can't open {graph_file}")
+            return
+
+        print("plotting")
+        fig, ax = ox.plot_graph(G)
+        print("showing")
+        plt.show()
