6dee4bf5352279020065f01db019eeb831daafa5
Sacha Wheeler <sacha@sachawheeler.com>
2024-07-31 09:06:46 +0100

    save point

--- pubcrawler/pubs/views.py
+++ pubcrawler/pubs/views.py
@@ -1,7 +1,7 @@
 from django.views import View
 from django.shortcuts import render
 from django.http import JsonResponse
-from django.contrib.gis.geos import Point
+from django.contrib.gis.geos import Point, Polygon
 from django.contrib.gis.measure import Distance as D
 from django.contrib.gis.db.models.functions import GeometryDistance
 # from django.contrib.gis.db.models.functions import Distance
@@ -31,6 +31,20 @@ class ShortestPathView(View):
             # Convert to Point objects
             start_point = Point(start_lon, start_lat)  # Note the order: lon, lat
             end_point = Point(end_lon, end_lat)
+            print(f"{start_point.x=}, {start_point.y=} - {end_point.x=}, {end_point.y=}")
+
+            padding = 0 #.01
+            min_lon = min(start_point.x, end_point.x) - padding
+            max_lon = max(start_point.x, end_point.x) + padding
+            min_lat = min(start_point.y, end_point.y) - padding
+            max_lat = max(start_point.y, end_point.y) + padding
+
+            bbox = (min_lon, min_lat, max_lon, max_lat)  #(xmin, ymin, xmax, ymax).
+            geom = Polygon.from_bbox(bbox)
+            print(bbox)
+
+
+            print(rf"{min_lat=}, {max_lat=}, {min_lon=}, {max_lon=}")
 
             # Find the nearest location to the start_point
             """
@@ -38,20 +52,35 @@ class ShortestPathView(View):
                 distance=Distance('location', start_point)
             ).order_by('distance').first()
             """
-            nearest_location = Pub.objects.filter(
+            pubs = Pub.objects.filter(
+                location__intersects=geom
+            )
+            print(f"{len(pubs)=}")
+            nearest_to_start = pubs.filter(
                 location__distance_lte=(
                     start_point,
-                    D(m=1000)
+                    D(m=2000)
                 )
             ).order_by(
                 GeometryDistance("location", start_point)
             ).first()
+            print(f"{nearest_to_start=}, {nearest_to_start.location}")
+            nearest_to_end = pubs.filter(
+                location__distance_lte=(
+                    end_point,
+                    D(m=1000)
+                )
+            ).order_by(
+                GeometryDistance("location", end_point)
+            ).first()
+            print(f"{nearest_to_end=}")
 
-            if not nearest_location:
-                return JsonResponse({"error": "No locations found in the database"}, status=404)
+            return
+
+            # if not nearest_location: return JsonResponse({"error": "No locations found in the database"}, status=404)
 
             # Get the coordinates of the nearest point
-            nearest_start_coords = (nearest_location.location.y, nearest_location.location.x)
+            nearest_start_coords = (nearest_to_start.location.y, nearest_to_start.location.x)
 
             # Use osmnx to download the map graph for the area surrounding the start and end points
             G = ox.graph_from_point(nearest_start_coords, dist=2000, network_type='walk')
